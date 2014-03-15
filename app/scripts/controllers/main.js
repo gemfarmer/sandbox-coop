2bd5ee85aef861ae8116ef067c1ae69a6d683c20
gemfarmer <bshslowfood@gmail.com>
2014-03-15 18:11:56 -0500

    adding admin

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,4 +5,8 @@ angular.module('sandboxCoop4App')
     $http.get('/api/awesomeThings').success(function(awesomeThings) {
       $scope.awesomeThings = awesomeThings;
     });
+    $http.get('/api/contributorData').success(function(contributorData) {
+    	console.log(contributorData)
+      $scope.contributorData = contributorData;
+    });
   });
