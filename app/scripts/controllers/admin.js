19a43a7ca32d874b4b48b8b63cf7277feb78de8e
gemfarmer <bshslowfood@gmail.com>
2014-03-16 01:41:20 -0500

    admin page fully restrictive

--- app/scripts/controllers/admin.js
+++ app/scripts/controllers/admin.js
@@ -2,10 +2,7 @@
 
 angular.module('sandboxCoop4App')
   .controller('AdminCtrl', function ($http, $scope, Auth, User, $location) {
-    console.log("$http: ",$http)
     $http.get('/api/admin/showAllUsers').success(function(data) {
-      console.log("adminData: ",data)
-      console.log("dataONe",data[0].name)
       $scope.userList = data;
     });
   });
