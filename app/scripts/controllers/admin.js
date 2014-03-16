d6e13667ac02b2f4fb80b58008b3f1c9aa5b16d5
gemfarmer <bshslowfood@gmail.com>
2014-03-16 14:35:47 -0500

    clean-up

--- app/scripts/controllers/admin.js
+++ app/scripts/controllers/admin.js
@@ -8,46 +8,8 @@ angular.module('sandboxCoop4App')
         $scope.toggleRole = !$scope.toggleRole;
         $scope.indexOfRole = $index;
         $scope.$watch('userList', function(choice) {
-            console.log(choice[$index]._id)
-
-            // $http.put('/api/users/:id', {id:choice[$index]._id, role:choice[$index].role}).success(function (data) {
-            //     console.log("response",data)
-            // });
             User.update({id:choice[$index]._id, role:choice[$index].role})
 
-
-
-            // return User.update({
-            //   _id:choice[$index]._id,
-            //   role:choice[$index].role
-            // }).$promise;
-            // User.get({id:choice[$index]._id},function(data) {
-            //         console.log(data)
-            //         console.log("data role before: ",data.profile.name,data.profile.role)
-            //        // $scope.data = data;
-
-            //        data.profile.role = choice[$index].role;
-            //         console.log("data role after: ",data.profile.role)
-            //        return data
-
-            //        // data.$save()
-            //     // });
-
-            // // var user = User.get({id:choice[$index]._id}, function() {
-
-            // });
-
-
-            // console.log("updated",updatedUser)
-            // updatedUser.$save(function(data,err){
-            //   console.log(data,err)
-            // });
-
-            // user.role = choice[$index].role;
-            // user.$save();
-            // console.log("user",user,"user profile", user.profile)
-            // user.profile.role = choice[$index].role;
-            // user.$save();
         }, true);
 
       } // end editRole()
--- app/scripts/services/updateUser.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('sandboxCoop4App')
-  .factory('UpdateUser', function ($resource) {
-    return $resource('/api/users/:id', {
-      id: '@id'
-    });
-  });
